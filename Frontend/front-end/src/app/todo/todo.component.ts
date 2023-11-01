0f1f5e38b2d5829bcb240fb217d1b8be28f14ce1
Solomon <solomonaustin02@gmail.com>
2023-11-01 12:47:00 +0530

    Made some changes

--- Frontend/front-end/src/app/todo/todo.component.ts
+++ Frontend/front-end/src/app/todo/todo.component.ts
@@ -2,14 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
-const BASE_API_URL = 'http://localhost:8000/users';
+const BASE_API_URL = 'http://localhost:8000/todos';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss']
 })
-export class todoComponent implements OnInit {
+export class TodoComponent implements OnInit {
   page: number = 1;
   pageSize: number = 5;
   userArray: any[] = [];
@@ -19,7 +19,7 @@ export class todoComponent implements OnInit {
   description: string = '';
   duedate: string = '';
 
-  showForm: boolean = false; 
+  showForm: boolean = false;
 
   constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) {}
 
@@ -32,8 +32,6 @@ export class todoComponent implements OnInit {
     });
 
     this.getAllData();
-    this.loadNextPage();
-    this.loadPreviousPage();
   }
 
   getAllData() {
@@ -62,32 +60,55 @@ export class todoComponent implements OnInit {
       });
   }
 
-  UpdateUser() {
+  save() {
+    if (this.currentuserID) {
+      this.updateUser();
+    } else {
+      this.register();
+    }
+  }
+
+  register() {
     const bodyData = {
       title: this.title,
       description: this.description,
       duedate: this.duedate,
     };
 
-    if (this.currentuserID) {
-      const apiUrl = `${BASE_API_URL}/${this.currentuserID}`;
-      this.http.patch(apiUrl, bodyData)
-        .subscribe((resultData: any) => {
-          console.log(resultData);
-          alert('Data Updated');
-          this.showForm = false;
+    this.http.post(BASE_API_URL, bodyData).subscribe(
+      (resultData: any) => {
+        console.log(resultData);
+        if (resultData && resultData.status === true) {
+          alert('Registered Successfully');
+          this.resetForm();
           this.getAllData();
-        });
-    } else {
-      const apiUrl = BASE_API_URL;
-      this.http.post(apiUrl, bodyData)
-        .subscribe((resultData: any) => {
-          console.log(resultData);
-          alert('Data Created');
-          this.showForm = false;
-          this.getAllData();
-        });
-    }
+        } else {
+          console.error('Invalid response from the server:', resultData);
+          alert('Error creating user. Please try again.');
+        }
+      },
+      (error) => {
+        console.error('Error creating user:', error);
+        alert('Error creating user. Please try again.');
+      }
+    );
+  }
+
+  updateUser() {
+    const bodyData = {
+      title: this.title,
+      description: this.description,
+      duedate: this.duedate,
+    };
+
+    const apiUrl = `${BASE_API_URL}/${this.currentuserID}`;
+    this.http.patch(apiUrl, bodyData)
+      .subscribe((resultData: any) => {
+        console.log(resultData);
+        alert('Data Updated');
+        this.resetForm();
+        this.getAllData();
+      });
   }
 
   loadNextPage() {
@@ -108,24 +129,7 @@ export class todoComponent implements OnInit {
     this.duedate = data.duedate;
 
     this.currentuserID = data._id;
-    this.showForm = true; 
-  }
-
-  UpdateRecords() {
-    let bodyData = {
-      title: this.title,
-      description: this.description,
-      duedate: this.duedate,
-    };
-
-    this.http.patch(`${BASE_API_URL}/${this.currentuserID}`, bodyData).subscribe(
-      (resultData: any) => {
-        console.log(resultData);
-        alert('Database Updateddd');
-        this.showForm = false; 
-        this.getAllData();
-      }
-    );
+    this.showForm = true;
   }
 
   setDelete(data: any) {
@@ -136,29 +140,12 @@ export class todoComponent implements OnInit {
     });
   }
 
-  save() {
-    if (this.currentuserID == '') {
-      this.register();
-    } else {
-      this.UpdateRecords();
-    }
-  }
-
-  register() {
-    let bodyData = {
-      title: this.title,
-      description: this.description,
-      duedate: this.duedate,
-    };
-
-    this.http.post(BASE_API_URL, bodyData).subscribe((resultData: any) => {
-      console.log(resultData);
-      alert('Registered Successfully');
-      this.showForm = false;
-      this.title = '';
-      this.description = '';
-      this.duedate = '';
-      this.getAllData();
-    });
+  resetForm() {
+    this.title = '';
+    this.description = '';
+    this.duedate = '';
+    this.currentuserID = '';
+    this.showForm = false;
   }
 }
+
